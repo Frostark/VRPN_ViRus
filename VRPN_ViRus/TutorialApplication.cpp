fae61db5f7b561976da17328823865ef7cd9db8e
Joaquín Monteagudo Gómez @ Tower <Kindos7@gmail.com>
2017-04-03 20:24:20 +0100

    Testing enviroment set up

--- VRPN_ViRus/TutorialApplication.cpp
+++ VRPN_ViRus/TutorialApplication.cpp
@@ -29,6 +29,9 @@ TutorialApplication::~TutorialApplication(void)
 //---------------------------------------------------------------------------
 void TutorialApplication::createScene(void)
 {
+	//Head's position
+	static const Ogre::Vector3 player_pos(0, 2, 0);
+
 	//Set up the scene manager on the map
 	ViRus::Hittable::ptr_scn_mgr = mSceneMgr;
 
@@ -42,16 +45,16 @@ void TutorialApplication::createScene(void)
 	Ogre::Camera* newCamera = mSceneMgr->createCamera("NewCam");
 	mWindow->getViewport(0)->setCamera(newCamera);
 	newCamera->setPosition(Ogre::Vector3(0, 0, 0));
-	newCamera->lookAt(Ogre::Vector3(0, 0, -1));
+	newCamera->lookAt(Ogre::Vector3(Ogre::Vector3::NEGATIVE_UNIT_Z));
 	newCamera->setNearClipDistance(0.1);
 
-	tracker = new vrpn_Tracker_Remote("iotracker@161.67.196.59:3883");
-	tracker->register_change_handler(this, handleHMDTracker);
+	//tracker = new vrpn_Tracker_Remote("iotracker@161.67.196.59:3883");
+	//tracker->register_change_handler(this, handleHMDTracker);
 
 	vrpnButton1 = new vrpn_Button_Remote("joyWin32_1@localhost");
-	vrpnButton2 = new vrpn_Button_Remote("joyWin32_2@localhost");
+	//vrpnButton2 = new vrpn_Button_Remote("joyWin32_2@localhost");
 	vrpnButton1->register_change_handler(this, handleButton1);
-	vrpnButton2->register_change_handler(this, handleButton2);
+	//vrpnButton2->register_change_handler(this, handleButton2);
 
 	//Init tracking data
 	vrpn_TRACKERCB blank = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 };
@@ -73,6 +76,8 @@ void TutorialApplication::createScene(void)
 	//Create the HMD node
 	HMDNode = mSceneMgr->getRootSceneNode()->createChildSceneNode("HMDNode");
 
+	HMDNode->setPosition(Ogre::Vector3::ZERO + player_pos);
+
 	//Attach an eye node to the HMD node, and translate the offset from the tracer's position to the eye position
 	Ogre::SceneNode *eyeNode = HMDNode->createChildSceneNode();
 	eyeNode->translate(0.0, -0.1, 0.05);
@@ -82,15 +87,19 @@ void TutorialApplication::createScene(void)
 	leftHandNode = mSceneMgr->getRootSceneNode()->createChildSceneNode("leftHandNode");
 	leftHandNode->attachObject(leftHandEntity);
 	leftHandNode->scale(Ogre::Vector3(1, 1, 1));
-	leftHandNode->pitch(Ogre::Degree(-90.0f));
-	leftHandNode->roll(Ogre::Degree(180.0f));
+	//leftHandNode->pitch(Ogre::Degree(-90.0f));
+	//leftHandNode->roll(Ogre::Degree(180.0f));
+
+	leftHandNode->setPosition(Ogre::Vector3(-0.2, -0.2, -0.4)+player_pos);
 
 	//Create the right hand node, and attach
 	rightHandNode = mSceneMgr->getRootSceneNode()->createChildSceneNode("rightHandNode");
 	rightHandNode->attachObject(rightHandEntity);
 	rightHandNode->scale(Ogre::Vector3(1, 1, 1));
-	rightHandNode->pitch(Ogre::Degree(-90.0f));
-	rightHandNode->roll(Ogre::Degree(180.0f));
+	//rightHandNode->pitch(Ogre::Degree(-90.0f));
+	//rightHandNode->roll(Ogre::Degree(180.0f));
+
+	rightHandNode->setPosition(Ogre::Vector3(+0.2, -0.2, -0.4)+player_pos);
 
 
 	// Create a Light and set its position
@@ -146,7 +155,8 @@ bool TutorialApplication::frameRenderingQueued(const Ogre::FrameEvent& evt)
 	bool ret = BaseApplication::frameRenderingQueued(evt);
 
 	//Tracker loops
-	tracker->mainloop();
+	//tracker->mainloop();
+	vrpnButton1->mainloop();
 
 	if (!processUnbufferedInput(evt)) return false;
 
@@ -211,14 +221,14 @@ void TutorialApplication::shootBullet(Ogre::SceneNode * scene)
 //Update nodes' positions based on trackers
 bool TutorialApplication::processUnbufferedInput(const Ogre::FrameEvent& evt)
 {
-	HMDNode->setPosition(Ogre::Vector3(HMDData.pos[0], HMDData.pos[1], HMDData.pos[2]));
-	HMDNode->setOrientation(HMDData.quat[3], HMDData.quat[0], HMDData.quat[1], HMDData.quat[2]);
+	//HMDNode->setPosition(Ogre::Vector3(HMDData.pos[0], HMDData.pos[1], HMDData.pos[2]));
+	//HMDNode->setOrientation(HMDData.quat[3], HMDData.quat[0], HMDData.quat[1], HMDData.quat[2]);
 
-	rightHandNode->setPosition(Ogre::Vector3(rightHandData.pos[0], rightHandData.pos[1], rightHandData.pos[2]));
-	rightHandNode->setOrientation(rightHandData.quat[3], rightHandData.quat[0], rightHandData.quat[1], rightHandData.quat[2]);
+	//rightHandNode->setPosition(Ogre::Vector3(rightHandData.pos[0], rightHandData.pos[1], rightHandData.pos[2]));
+	//rightHandNode->setOrientation(rightHandData.quat[3], rightHandData.quat[0], rightHandData.quat[1], rightHandData.quat[2]);
 
-	leftHandNode->setPosition(Ogre::Vector3(leftHandData.pos[0], leftHandData.pos[1], leftHandData.pos[2]));
-	leftHandNode->setOrientation(leftHandData.quat[3], leftHandData.quat[0], leftHandData.quat[1], leftHandData.quat[2]);
+	//leftHandNode->setPosition(Ogre::Vector3(leftHandData.pos[0], leftHandData.pos[1], leftHandData.pos[2]));
+	//leftHandNode->setOrientation(leftHandData.quat[3], leftHandData.quat[0], leftHandData.quat[1], leftHandData.quat[2]);
 
 
 	// Update Bullet Physics animation

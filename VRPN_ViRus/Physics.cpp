5b0ed69efd3f97277c4e289f8785d03572869d22
Joaquín Monteagudo Gómez @ Tower <Kindos7@gmail.com>
2017-05-14 18:04:58 +0200

    Added death and walk animations

--- VRPN_ViRus/Physics.cpp
+++ VRPN_ViRus/Physics.cpp
@@ -189,14 +189,57 @@ namespace ViRus
 			//Check if they're enemies
 			if (attack_team(*ptr_hitcharacter))
 			{
-				//Do damage
-				if (deltaAttack == 0.0)
+				Ogre::Entity *ent = get_entity();
+				
+				if ((ent && anim != CharacterAnimState::DEATH && anim != CharacterAnimState::END) || !ent)
 				{
-					ptr_hitcharacter->takeDamage(dmg);
-					deltaAttack = timeAttack;
+					//Do damage
+					if (deltaAttack == 0.0)
+					{
+						ptr_hitcharacter->takeDamage(dmg);
+						deltaAttack = timeAttack;
+
+						if (ent)
+						{
+							anim = CharacterAnimState::ATTACK;
+							anim_controller = ent->getAnimationState("Attack2");
+							anim_controller->setLoop(false);
+							anim_controller->setEnabled(true);
+						}
+					}
 				}
 			}
 		}
+	}
+
+	bool HitCharAttack::finished()
+	{
+		if (HitCharacter::finished())
+		{
+			Ogre::Entity *ent = get_entity();
+
+			if (ent)
+			{
+				switch (anim)
+				{
+					case CharacterAnimState::END:
+						return true;
+					case CharacterAnimState::DEATH:
+						return false;
+					default:
+					{
+						anim = CharacterAnimState::DEATH;
+						anim_controller = ent->getAnimationState("Death1");
+						anim_controller->setLoop(false);
+						anim_controller->setEnabled(true);
+						return false;
+					}
+				}
+			}
+			else
+				return true;
+		}
+		return false;
 	}
 
 	void HitCharAttack::deltaTime(double itime)
@@ -208,15 +251,26 @@ namespace ViRus
 			{
 				case ViRus::CharacterAnimState::START:
 					anim = CharacterAnimState::WALK;
-					anim_controller = entity->getAnimationState("Walk");
+					anim_controller = ent->getAnimationState("Walk");
 					anim_controller->setLoop(true);
 					anim_controller->setEnabled(true);
 					break;
 				case ViRus::CharacterAnimState::WALK:
 					break;
 				case ViRus::CharacterAnimState::ATTACK:
+					if (anim_controller->hasEnded())
+					{
+						anim = CharacterAnimState::WALK;
+						anim_controller = ent->getAnimationState("Walk");
+						anim_controller->setLoop(true);
+						anim_controller->setEnabled(true);
+					}
 					break;
 				case ViRus::CharacterAnimState::DEATH:
+					if (anim_controller->hasEnded())
+					{
+						anim = CharacterAnimState::END;
+					}
 					break;
 				case ViRus::CharacterAnimState::END:
 					break;
@@ -251,9 +305,18 @@ namespace ViRus
 	}
 	void HitCharAttack::chase(const HitCharacter & h)
 	{
-		Ogre::Vector3 pos;
-		if (h.get_position(pos))
-			go_point(pos);
+		Ogre::Entity *ent = get_entity();
+		if ((ent && anim != CharacterAnimState::DEATH && anim != CharacterAnimState::END) || !ent)
+		{
+			Ogre::Vector3 pos;
+			if (h.get_position(pos))
+				go_point(pos);
+		}
+		else
+		{
+			if (body)
+				body->setLinearVelocity(Ogre::Vector3::ZERO);
+		}
 	}
 
 	Ogre::Entity * HitCharAttack::get_entity()
